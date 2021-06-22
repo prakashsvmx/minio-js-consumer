de7bc87fc111c8d9155f7c208be79ced6bf0c3a1
prakashsvmx <prakash@minio.io>
2021-06-22 17:05:21 +0530

    List object v2 demo

---
+++ index.js
@@ -0,0 +1,22 @@
+var Minio = require('minio')
+
+var minioClient = new Minio.Client({
+    endPoint: 'localhost',
+    port: 9000,
+    useSSL: false,
+    accessKey: 'minio',
+    secretKey: 'minio123'
+});
+
+
+function listObjectV2(){
+    var bucketName = "sph-my-bucket"
+
+    const fGetPromise = minioClient.listObjectsV2(bucketName, "/test", true)
+
+    fGetPromise.on('data', function(obj) { console.log(obj) } )
+    fGetPromise.on('error', function(err) { console.log(err) } )
+
+}
+
+listObjectV2()
